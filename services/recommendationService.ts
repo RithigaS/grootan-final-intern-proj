569d88d900c7e8e74d8891c25ddc2ed329bfed5f
Rithiga.S <rithiga.s@grootan.com>
2026-06-30 22:31:47 +0530

    updated url

--- services/recommendationService.ts
+++ services/recommendationService.ts
@@ -1,9 +1,12 @@
 import { Recommendation } from "@/types/recommendation";
 
 export async function getRecommendations(): Promise<Recommendation[]> {
-    const res = await fetch("process.env.NEXT_PUBLIC_APP_URL/api/recommendations", {
-        cache: "no-store",
-    });
+    const res = await fetch(
+        `${process.env.NEXT_PUBLIC_APP_URL}/api/recommendations`,
+        {
+            cache: "no-store",
+        }
+    );
 
     if (!res.ok) {
         throw new Error("Failed to fetch recommendations");
